372d027c387621b1a160b9c6eb62702c8ebc4b49
Scott Lam <scott.lam7344@yahoo.com>
2023-02-07 18:48:33 -0600

    Image Upload functionality

--- landing-page-prototype/src/pages/LoginPage.js
+++ landing-page-prototype/src/pages/LoginPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ToggleMode from '../components/ToggleMode';
+import ImageUploader from '../components/ImageUploader';
 import './LoginPage.css'
 
 function LoginPage() {
 
     return (
         <div className='login-test'>
-            <h1>Login Page</h1>
+            <h1>Login Page (Test Page)</h1>
+            <ImageUploader />
             <ToggleMode />
         </div>
     )
