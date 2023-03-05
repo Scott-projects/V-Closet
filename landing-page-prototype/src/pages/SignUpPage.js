ecd9fd793114b5464f255a53960ea7dcbf052f7a
Scott Lam <scott.lam7344@yahoo.com>
2023-03-05 00:06:31 -0600

    Stylized text inputs and added labels

--- landing-page-prototype/src/pages/SignUpPage.js
+++ landing-page-prototype/src/pages/SignUpPage.js
@@ -6,7 +6,7 @@ import TopNavBar from '../components/TopNavBar';
 import { signUpwithEmailAndPassword, googleSignIn } from '../firebase/authentication';
 import { signInWithPopup } from 'firebase/auth';
 
-const SignUpPage =() => {
+const SignUpPage = () => {
     //States for user input
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -16,40 +16,53 @@ const SignUpPage =() => {
     // Handles the user submit flow
     const handleFlow = async (expectedDefault) => {
         expectedDefault.preventDefault();
-        if(password !== passwordCheck){
+        if (password !== passwordCheck) {
             setError("Both passwords must match! Please make sure you enter the same password.");
         } else {
             setEmail("");
             setPassword("");
             setPasswordCheck("");
             const result = await signUpwithEmailAndPassword(email, password);
-            if(result.error){
+            if (result.error) {
                 setError(result.error);
             }
         }
-    }; 
+    };
 
     return (
         <>
-            <div className='signup'>
-                <div className='overlay'></div>
-                <video className='formVideo' src={videobg2} autoPlay muted />
-                <div className='formShape'>
-                    <h2>Sign Up Now!</h2>
-                    {error ? <div>{error}</div> : null}
-                    <p>Please enter your username and password below:</p>
-                    <form onSubmit={handleFlow}>
-                        <input type="email" name="email" value={email} placeholder="yourEmail@provider.com" required onChange={(change) => setEmail(change.target.value)} />
-                        <input type="password" name="password" value={password} placeholder="Your password" required onChange={(change) => setPassword(change.target.value)} />
-                        <input type="password" name="passwordCheck" value={passwordCheck} placeholder="Re-enter your password" required onChange={(change) => setPasswordCheck(change.target.value)} />
-                        <button type="submit">Submit</button>
-                    </form>
-                    <div>
-                        <button onClick={googleSignIn}>Sign in with google</button>
+            <body className='overflow'>
+                <div className='signup'>
+                    <div className='overlay'></div>
+                    <video className='formVideo' src={videobg2} autoPlay muted />
+                    <div className='formShape'>
+                        <h2 className='header-text'>CREATE ACCOUNT</h2>
+                        {error ? <div>{error}</div> : null}
+                        {/* <p>Please enter your username and password below:</p> */}
+                        <form onSubmit={handleFlow}>
+                            <div className='form-group'>
+                                <label for="emailLabel">Email</label>
+                                <input type="email" name="email" value={email} placeholder="yourEmail@provider.com" required onChange={(change) => setEmail(change.target.value)} />
+                            </div>
+
+                            <div className='form-group'>
+                                <label for="passwordLabel">Password</label>
+                                <input type="password" name="password" value={password} placeholder="Your password" required onChange={(change) => setPassword(change.target.value)} />
+                            </div>
+
+                            <div className='form-group'>
+                                <label for="passwordCheckLabel">Re-enter Password</label>
+                                <input type="password" name="passwordCheck" value={passwordCheck} placeholder="Re-enter your password" required onChange={(change) => setPasswordCheck(change.target.value)} />
+                            </div>
+                            <button type="submit">Submit</button>
+                        </form>
+                        <div>
+                            <button onClick={googleSignIn}>Sign in with google</button>
+                        </div>
+                        <p>Are you already registred with us? TODO: IMPLEMENT ROUTER TO LOGIN!</p>
                     </div>
-                    <p>Are you already registred with us? TODO: IMPLEMENT ROUTER TO LOGIN!</p>
                 </div>
-            </div>
+            </body>
         </>
     );
 };
