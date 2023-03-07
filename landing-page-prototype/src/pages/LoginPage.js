3a1a3ca7dd7cde9294fbe72b3978f8ef6d68f14a
Scott Lam <scott.lam7344@yahoo.com>
2023-03-07 12:05:58 -0600

    Stylized LoginPage.js

--- landing-page-prototype/src/pages/LoginPage.js
+++ landing-page-prototype/src/pages/LoginPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useState, Dialog } from 'react';
-import TopNavBar from '../components/TopNavBar';
 import videobg2 from '../assets/video2.mp4'
+import { FaGoogle } from 'react-icons/fa';
 import '../styles/LoginPage.css';
 import { emailSignIn, googleSignIn } from '../firebase/authentication';
 
@@ -14,23 +15,33 @@ function LoginPage() {
         setEmail("");
         setPassword("");
         const result = await googleSignIn();
-        if(result.error){
+        if (result.error) {
             setError(result.error);
         }
     }
     return (
         <div className='login'>
             <div className='overlay'></div>
-            <video className='formVideo' src={videobg2} autoPlay muted/>
+            <video className='formVideo' src={videobg2} autoPlay muted />
             <div className='formShape'>
-                <h2>Sign in</h2>
+                <h2 className='header-text'>Login</h2>
                 {error ? <div>{error}</div> : null}
                 <form onSubmit={handleFlow}>
-                    <input type="email" name="email" value={email} placeholder="yourEmail@provider.com" required onChange={(change) => setEmail(change.target.value)} />
-                    <input type="password" name="password" value={password} placeholder="Your password" required onChange={(change) => setPassword(change.target.value)} />
-                    <button type="submit">Submit</button>
+                    <div className='form-group'>
+                        <label for="emailLabel">Email</label>
+                        <input type="email" name="email" value={email} placeholder="yourEmail@provider.com" required onChange={(change) => setEmail(change.target.value)} />
+                    </div>
+
+                    <div className='form-group'>
+                        <label for="passwordLabel">Password</label>
+                        <input type="password" name="password" value={password} placeholder="Your password" required onChange={(change) => setPassword(change.target.value)} />
+                    </div>
+                    <button type="submit" className="submitLogin">Login</button>
                 </form>
-                <button onClick={googleSignIn}>Sign in with google</button>
+                <div>
+                    <button onClick={googleSignIn} className="googleSignUp"><FaGoogle className='FaGoogle' /> Sign in with Google</button>
+                </div>
+                <p className='loginToRegister'>Not a Member? <Link to="/signup" className='linkToSignUp'>Click Here</Link></p>
             </div>
         </div>
     )
--- landing-page-prototype/src/pages/SignUpPage.js
+++ landing-page-prototype/src/pages/SignUpPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import videobg2 from '../assets/video2.mp4';
-import {FaGoogle} from 'react-icons/fa';
+import { FaGoogle } from 'react-icons/fa';
 import '../styles/SignUpPage.css';
 import { signUpwithEmailAndPassword, googleSignIn } from '../firebase/authentication';
 import { signInWithPopup } from 'firebase/auth';
@@ -32,38 +32,37 @@ const SignUpPage = () => {
 
     return (
         <>
-            <body className='overflow'>
-                <div className='signup'>
-                    <div className='overlay'></div>
-                    <video className='formVideo' src={videobg2} autoPlay muted />
-                    <div className='formShape'>
-                        <h2 className='header-text'>CREATE ACCOUNT</h2>
-                        {error ? <div>{error}</div> : null}
-                        {/* <p>Please enter your username and password below:</p> */}
-                        <form onSubmit={handleFlow}>
-                            <div className='form-group'>
-                                <label for="emailLabel">Email</label>
-                                <input type="email" name="email" value={email} placeholder="yourEmail@provider.com" required onChange={(change) => setEmail(change.target.value)} />
-                            </div>
-
-                            <div className='form-group'>
-                                <label for="passwordLabel">Password</label>
-                                <input type="password" name="password" value={password} placeholder="Your password" required onChange={(change) => setPassword(change.target.value)} />
-                            </div>
-
-                            <div className='form-group'>
-                                <label for="passwordCheckLabel">Re-enter Password</label>
-                                <input type="password" name="passwordCheck" value={passwordCheck} placeholder="Re-enter your password" required onChange={(change) => setPasswordCheck(change.target.value)} />
-                            </div>
-                            <button type="submit" className="submitSignUp">Sign Up</button>
-                        </form>
-                        <div>
-                            <button onClick={googleSignIn} className="googleSignUp"><FaGoogle className='FaGoogle'/> Sign in with Google</button>
+            <div className='signup'>
+                <div className='overlay'></div>
+                <video className='formVideo' src={videobg2} autoPlay muted />
+                <div className='formShape'>
+                    <h2 className='header-text'>CREATE ACCOUNT</h2>
+                    {error ? <div>{error}</div> : null}
+                    {/* <p>Please enter your username and password below:</p> */}
+                    <form onSubmit={handleFlow}>
+                        <div className='form-group'>
+                            <label for="emailLabel">Email</label>
+                            <input type="email" name="email" value={email} placeholder="yourEmail@provider.com" required onChange={(change) => setEmail(change.target.value)} />
                         </div>
-                        <p className='registerToLogin'>Already registered with us? <Link to="/login" className='linkToLogin'>Click Here</Link></p>
+
+                        <div className='form-group'>
+                            <label for="passwordLabel">Password</label>
+                            <input type="password" name="password" value={password} placeholder="Your password" required onChange={(change) => setPassword(change.target.value)} />
+                        </div>
+
+                        <div className='form-group'>
+                            <label for="passwordCheckLabel">Re-enter Password</label>
+                            <input type="password" name="passwordCheck" value={passwordCheck} placeholder="Re-enter your password" required onChange={(change) => setPasswordCheck(change.target.value)} />
+                        </div>
+                        <button type="submit" className="submitSignUp">Sign Up</button>
+                    </form>
+                    <div>
+                        <button onClick={googleSignIn} className="googleSignUp"><FaGoogle className='FaGoogle' /> Sign in with Google</button>
                     </div>
+                    <p className='registerToLogin'>Already registered with us? <Link to="/login" className='linkToLogin'>Click Here</Link></p>
                 </div>
-            </body>
+            </div>
+
         </>
     );
 };
