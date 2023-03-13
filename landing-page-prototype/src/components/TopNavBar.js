0743a33c312aaf33acadc5c8f476b5d4c40f975d
Scott Lam <scott.lam7344@yahoo.com>
2023-03-12 22:28:59 -0500

    Add Wardrobe Icon to Navbar

--- landing-page-prototype/src/components/TopNavBar.js
+++ landing-page-prototype/src/components/TopNavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { AiOutlineHome } from 'react-icons/ai';
 import { BsGear } from 'react-icons/bs';
+import { GiClothes } from 'react-icons/gi';
 import ToggleMode from './ToggleMode';
 import '../styles/TopNavBar.css';
 
@@ -10,13 +11,16 @@ const TopNavBar = () => {
         <nav className='navbar-menu'>
             <ul className='navbar-nav'>
                 <li className='navbar-home'>
-                    <Link className='homeIcon' to="/"><AiOutlineHome /></Link>
+                    <Link className='homeIcon' to="/home"><AiOutlineHome /></Link>
                 </li>
                 <li className='navbar-gear'>
                     <Link className='settingsIcon' to="/settings"><BsGear /></Link>
                 </li>
                 <li className='navbar-toggle'>
                     <ToggleMode />
+                </li>
+                <li className='navbar-clothes'>
+                    <Link className='clothesIcon' to="/wardrobe"><GiClothes /></Link>
                 </li>
             </ul>
         </nav>
