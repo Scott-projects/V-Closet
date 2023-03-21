99960f92d01391257773e3ad7c3a6cf9c27be8fa
Tsimmsz <78185426+Tsimmsz@users.noreply.github.com>
2023-03-21 13:37:45 -0500

    cleaning sign up

--- landing-page-prototype/src/components/SignUpSystem.js
+++ landing-page-prototype/src/components/SignUpSystem.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import '../styles/SignUpSystem.css';
-import { FaGoogle } from 'react-icons/fa';
 import { signUpwithEmailAndPassword, googleSignIn } from '../firebase/authentication';
-import { signInWithPopup } from 'firebase/auth';
+import { auth } from '../firebase/firebase';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
 
 const SignUpSystem = () => {
     //States for user input
@@ -12,8 +12,17 @@ const SignUpSystem = () => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const [passwordCheck, setPasswordCheck] = useState("");
-
+    const [user, loading, authError] = useAuthState(auth);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (loading) {
+            return;
+        }
+        if (user) {
+            navigate("/wardrobe");
+        }
+    }, [user, loading, navigate]);
 
     // Handles the user submit flow
     const handleFlow = async (expectedDefault) => {
@@ -21,53 +30,49 @@ const SignUpSystem = () => {
         if (password !== passwordCheck) {
             setError("Both passwords must match! Please make sure you enter the same password.");
         } else {
+            setError("")
             setEmail("");
             setPassword("");
             setPasswordCheck("");
-
-            try {
-                await signUpwithEmailAndPassword(email, password);
-                console.log("Successful");
-                navigate("/home");
+            const result = await signUpwithEmailAndPassword(email, password);
+            if (result.error) {
+                setError(result.error);
             }
-            catch (error) {
-                console.log("Failed");
+            else {
+                navigate("/wardrobe");
             }
-            // if (result.error) {
-            //     // setError(result.error);
-            //     console.log(result.error);
-            // }
-            // else {
-            //     console.log("Test successful");
-            // }
         }
     };
 
     return (
-        <div>
-            {error ? <div>{error}</div> : null}
-            {/* <p>Please enter your username and password below:</p> */}
-            <form onSubmit={handleFlow}>
-                <div className='form-group'>
-                    <label for="emailLabel">Email</label>
-                    <input type="email" name="email" value={email} placeholder="yourEmail@provider.com" required onChange={(change) => setEmail(change.target.value)} />
-                </div>
+        <div className='signup'>
+            <div className='overlay'></div>
+            <div className='formShape'>
+                <h2 className='header-text'>CREATE ACCOUNT</h2>
+                {error ? <div>{error}</div> : null}
+                {authError ? <div>{authError}</div> : null}
+                <form onSubmit={handleFlow}>
+                    <div className='form-group'>
+                        <label for="emailLabel">Email</label>
+                        <input type="email" name="email" value={email} placeholder="yourEmail@provider.com" required onChange={(change) => setEmail(change.target.value)} />
+                    </div>
 
-                <div className='form-group'>
-                    <label for="passwordLabel">Password</label>
-                    <input type="password" name="password" value={password} placeholder="Your password" required onChange={(change) => setPassword(change.target.value)} />
-                </div>
+                    <div className='form-group'>
+                        <label for="passwordLabel">Password</label>
+                        <input type="password" name="password" value={password} placeholder="Your password" required onChange={(change) => setPassword(change.target.value)} />
+                    </div>
 
-                <div className='form-group'>
-                    <label for="passwordCheckLabel">Re-enter Password</label>
-                    <input type="password" name="passwordCheck" value={passwordCheck} placeholder="Re-enter your password" required onChange={(change) => setPasswordCheck(change.target.value)} />
+                    <div className='form-group'>
+                        <label for="passwordCheckLabel">Re-enter Password</label>
+                        <input type="password" name="passwordCheck" value={passwordCheck} placeholder="Re-enter your password" required onChange={(change) => setPasswordCheck(change.target.value)} />
+                    </div>
+                    <button type="submit" className="submitSignUp">Sign Up</button>
+                </form>
+                <div>
+                    <button onClick={googleSignIn} className="googleSignUp"><FaGoogle className='FaGoogle' /> Sign in with Google</button>
                 </div>
-                <button type="submit" className="submitSignUp">Sign Up</button>
-            </form>
-            <div>
-                <button onClick={googleSignIn} className="googleSignUp"><FaGoogle className='FaGoogle' /> Sign in with Google</button>
+                <p className='registerToLogin'>Already registered with us? <Link to="/login" className='linkToLogin'>Click Here</Link></p>
             </div>
-            <p className='registerToLogin'>Already registered with us? <Link to="/login" className='linkToLogin'>Click Here</Link></p>
         </div>
     )
 };
