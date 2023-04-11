51de4b4508825829c046b3b39c4bd1dc096315fc
Scott Lam <scott.lam7344@yahoo.com>
2023-04-10 21:06:29 -0500

    Made changes to SideBar

--- landing-page-prototype/src/components/SideBar.js
+++ landing-page-prototype/src/components/SideBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoAddCircleOutline } from "react-icons/io5" 
 import "../styles/SideBar.css";
 
 const SideBar = () => {
@@ -7,10 +8,14 @@ const SideBar = () => {
         <div className="sidebar">
             <h2>My Closet</h2>
             <ul className="side-categories">
+                <li className="side-item">All</li>
+                <li className="side-item">Outerwear</li>
+                <li className="side-item">Headwear</li>
+                <li className="side-item">Sweaters</li>
                 <li className="side-item">Shirts</li>
                 <li className="side-item">Pants</li>
                 <li className="side-item">Shoes</li>
-                <li className="side-item"><Link className="ToAddItem" to="/additem">Add Item (+)</Link></li>
+                <li className="side-item"><Link className="ToAddItem" to="/additem"><IoAddCircleOutline /></Link></li>
             </ul>
         </div>
     )
